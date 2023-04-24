1cd1e17f5eb733829d757232d2196a83433f299f
hkim1016 <hankim0827@gmail.com>
2023-04-23 22:23:50 -0700

    Testing performance optimization

--- src/World/systems/renderer.js
+++ src/World/systems/renderer.js
@@ -1,7 +1,12 @@
 import { WebGLRenderer } from "three";
 
 function createRenderer() {
-    const renderer = new WebGLRenderer();
+    const renderer = new WebGLRenderer({
+        alpha: false,
+        stencil: false,
+        preserveDrawingBuffer: false,
+        powerPreference: "high-performance",
+    });
 
     return renderer;
 }
